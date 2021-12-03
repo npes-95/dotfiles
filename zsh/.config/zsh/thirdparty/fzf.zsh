fbae6fb835267995a71b5e2fd9f992878489c645
npes-95 <npe.smith.95@gmail.com>
2021-12-03 13:06:29 +0100

    Add preview to fzf search.

--- zsh/.config/zsh/thirdparty/fzf.zsh
+++ zsh/.config/zsh/thirdparty/fzf.zsh
@@ -17,3 +17,19 @@ _fzf_compgen_path() {
 _fzf_compgen_dir() {
   fd --type d --hidden --follow --exclude ".git" . "$1"
 }
+
+# (EXPERIMENTAL) Advanced customization of fzf options via _fzf_comprun function
+# - The first argument to the function is the name of the command.
+# - You should make sure to pass the rest of the arguments to fzf.
+_fzf_comprun() {
+  local command=$1
+  shift
+
+  case "$command" in
+    cd)           fzf "$@" --preview 'tree -C {} | head -200' ;;
+    export|unset) fzf "$@" --preview "eval 'echo \$'{}" ;;
+    vim|nvim)     fzf "$@" --preview "cat {}"      ;;
+    ssh)          fzf "$@" --preview 'dig {}' ;;
+    *)            fzf "$@" ;;
+  esac
+}
