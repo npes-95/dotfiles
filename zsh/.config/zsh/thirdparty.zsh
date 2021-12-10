bf959b33d385965a4d5ab563a70ff43b39ad3cba
npes-95 <npe.smith.95@gmail.com>
2021-12-10 22:39:15 +0100

    Add git aliases.

--- zsh/.config/zsh/thirdparty.zsh
+++ zsh/.config/zsh/thirdparty.zsh
@@ -5,6 +5,7 @@ thirdparty_dir="$HOME/.config/zsh/thirdparty"
 has brew && source $thirdparty_dir/brew.zsh
 has cargo && source $thirdparty_dir/cargo.zsh
 has fzf && source $thirdparty_dir/fzf.zsh
+has git && source $thirdparty_dir/git.zsh
 has nvim && source $thirdparty_dir/nvim.zsh
 has tmux && source $thirdparty_dir/tmux.zsh
 has yabai && source $thirdparty_dir/yabai.zsh
---
+++ zsh/.config/zsh/thirdparty/git.zsh
@@ -0,0 +1,6 @@
+# ~/.config/zsh/thirdparty/git.zsh
+
+alias ga="git add"
+alias gc="git commit"
+alias gd="git diff"
+alias gs="git status"
