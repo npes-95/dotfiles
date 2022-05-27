e3ca41d0d781e51c77fab0bb1430d9c11c71bc1d
npes-95 <npe.smith.95@gmail.com>
2022-05-27 19:37:34 +0200

    zsh: drop yabai from thirdparty.zsh

--- zsh/.config/zsh/thirdparty.zsh
+++ zsh/.config/zsh/thirdparty.zsh
@@ -8,4 +8,3 @@ has fzf && source $thirdparty_dir/fzf.zsh
 has git && source $thirdparty_dir/git.zsh
 has nvim && source $thirdparty_dir/nvim.zsh
 has tmux && source $thirdparty_dir/tmux.zsh
-has yabai && source $thirdparty_dir/yabai.zsh
