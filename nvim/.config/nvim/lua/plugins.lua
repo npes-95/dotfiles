85e5068b2ca7192321e15d7567c93a0052fe4d9f
npes-95 <npe.smith.95@gmail.com>
2022-08-28 18:19:10 +0200

    nvim: robustify and cleanup plugin setup.

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -15,14 +15,6 @@ if fn.empty(fn.glob(install_path)) > 0 then
   vim.cmd [[packadd packer.nvim]]
 end
 
---Autocommand that reloads neovim whenever you save the plugins.lua file
-vim.cmd [[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerSync
-  augroup end
-]]
-
 --Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
@@ -41,23 +33,36 @@ packer.init {
 return packer.startup(function(use)
   --plugin manager
   use "wbthomason/packer.nvim" --have packer manage itself
+
   --utils
-  use "nvim-lua/popup.nvim" --an implementation of the Popup API from vim in Neovim
-  use "nvim-lua/plenary.nvim" --useful lua functions used by lots of plugins
+  use {
+    "nvim-lua/popup.nvim", --an implementation of the popup API from vim in Neovim
+    requires = { { "nvim-lua/plenary.nvim" } }
+  }
   use "lewis6991/impatient.nvim" --improve startup time
   use "antoinemadec/FixCursorHold.nvim" --needed to fix lsp doc highlight
+
   --qol
+  use {
+    "kyazdani42/nvim-tree.lua", -- file tree
+    requires = { { "kyazdani42/nvim-web-devicons" } } -- optional, for file icons
+  }
+  use {
+    "nvim-telescope/telescope.nvim", tag = "0.1.*", -- fuzzy finder
+    requires = { { "nvim-lua/plenary.nvim" } }
+  }
+  use "folke/which-key.nvim" --keymap prompt
+  use { "akinsho/toggleterm.nvim", tag = "v2.*" } --terminal window manager
   use "windwp/nvim-autopairs" --autopairs, integrates with both cmp and treesitter
   use "numToStr/Comment.nvim" --easily comment stuff
-  use "kyazdani42/nvim-web-devicons"
-  use "kyazdani42/nvim-tree.lua" --file tree
-  use "akinsho/toggleterm.nvim" --terminal window manager
-  use "folke/which-key.nvim" --keymap prompt
-  use "nvim-telescope/telescope.nvim" --fuzzy finder
-  use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' } --fzf extension for telescope
-  use "lewis6991/gitsigns.nvim" --git integration
+  use { "lewis6991/gitsigns.nvim", tag = "release" } --git integration
+
   --pretty
-  use "nvim-lualine/lualine.nvim" --nice status bar
+  use {
+    "nvim-lualine/lualine.nvim", --nice status bar
+    requires = { "kyazdani42/nvim-web-devicons", opt = true }
+  }
+
   --cmp
   use "hrsh7th/nvim-cmp" --the completion plugin
   use "hrsh7th/cmp-buffer" --buffer completions
@@ -65,22 +70,26 @@ return packer.startup(function(use)
   use "hrsh7th/cmp-cmdline" --cmdline completions
   use "saadparwaiz1/cmp_luasnip" --snippet completions
   use "hrsh7th/cmp-nvim-lsp"
+
   --snippets
   use "L3MON4D3/LuaSnip" --snippet engine
   use "rafamadriz/friendly-snippets" --a bunch of snippets to use
+
   --lsp
   use "neovim/nvim-lspconfig" --enable lsp
   use "williamboman/nvim-lsp-installer" --simple to use language server installer
   use "tamago324/nlsp-settings.nvim" --language server settings defined in json format
+
   --treesitter
   use {
     "nvim-treesitter/nvim-treesitter",
     run = ":TSUpdate",
   }
+
   --colorscheme
-  use {'kdheepak/monochrome.nvim', config = function()
-    vim.cmd 'colorscheme monochrome'
-  end}
+  use { "kdheepak/monochrome.nvim", config = function()
+    vim.cmd "colorscheme monochrome"
+  end }
 
   --Automatically set up your configuration after cloning packer.nvim
   --Put this at the end after all plugins
