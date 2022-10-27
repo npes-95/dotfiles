6f7614c4f5d1b508bf9cbbae7f6129b07a8dffca
npes-95 <npe.smith.95@gmail.com>
2022-10-27 22:41:55 +0200

    zsh/mpv: add aliases to launch nts

--- zsh/.config/zsh/thirdparty.zsh
+++ zsh/.config/zsh/thirdparty.zsh
@@ -7,6 +7,7 @@ has fnm && source $thirdparty_dir/fnm.zsh
 has fzf && source $thirdparty_dir/fzf.zsh
 has git && source $thirdparty_dir/git.zsh
 has launchctl && source $thirdparty_dir/launchctl.zsh
+has mpv && source $thirdparty_dir/mpv.zsh
 has nvim && source $thirdparty_dir/nvim.zsh
 has restic && source $thirdparty_dir/restic.zsh
 has rsync && source $thirdparty_dir/rsync.zsh
---
+++ zsh/.config/zsh/thirdparty/mpv.zsh
@@ -0,0 +1,2 @@
+alias nts="mpv 'https://stream-relay-geo.ntslive.net/stream'"
+alias nts2="mpv 'https://stream-relay-geo.ntslive.net/stream2'"
