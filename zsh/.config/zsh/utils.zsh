ac3d59cd4a496b75a24c360617f3990c3fba3505
npes-95 <npe.smith.95@gmail.com>
2022-10-27 22:41:16 +0200

    zsh: add helpers.

--- zsh/.config/zsh/utils.zsh
+++ zsh/.config/zsh/utils.zsh
@@ -3,12 +3,24 @@ has () {
 }
 
 reload () {
-    # see https://github.com/ohmyzsh/ohmyzsh/wiki/FAQ#how-do-i-reload-the-zshrc-file
-    exec zsh
+    exec $SHELL
 }
 
 prof () {
     for i in {1..10}; do time zsh -i -c exit; done
 }
 
+zconf () {
+  pushd $zsh_config
+  $EDITOR $zsh_config/init.zsh
+  popd
+}
 
+mksh () {
+  local filepath=$1
+  local desc=$2
+  local name=${path##*/}
+  touch $filepath
+  echo -e "#!bin/bash\n\n\# $name - $desc" > $filepath
+  chmod +x $filepath
+}
