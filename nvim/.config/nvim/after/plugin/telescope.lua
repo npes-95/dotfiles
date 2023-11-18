50426585bb563f6155906af53a7bc87dc636cacf
npes-95 <npe.smith.95@gmail.com>
2023-11-18 22:11:54 +0100

    nvim/telescope: search buffers instead of git branches

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -4,6 +4,6 @@ vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fm', builtin.man_pages, {})
 vim.keymap.set('n', '<leader>fk', builtin.keymaps, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 
 vim.keymap.set('n', '<leader>fc', builtin.git_commits, {})
-vim.keymap.set('n', '<leader>fb', builtin.git_branches, {})
