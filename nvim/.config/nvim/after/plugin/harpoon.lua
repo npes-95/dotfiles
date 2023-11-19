712b4e9bea5eb6faea82e5f0affb4540fe333bcb
npes-95 <npe.smith.95@gmail.com>
2023-11-19 01:13:01 +0100

    nvim/harpoon: add keybinding to navigate between marks

--- nvim/.config/nvim/after/plugin/harpoon.lua
+++ nvim/.config/nvim/after/plugin/harpoon.lua
@@ -3,3 +3,6 @@ local ui = require("harpoon.ui")
 
 vim.keymap.set("n", "<leader>ha", mark.add_file)
 vim.keymap.set("n", "<leader>hh", ui.toggle_quick_menu)
+
+vim.keymap.set("n", "<S-l>", ui.nav_next)
+vim.keymap.set("n", "<S-h>", ui.nav_prev)
