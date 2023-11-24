724310c7ad01cf15dd1f4d4c92b0862c3e3b26dd
npes-95 <npe.smith.95@gmail.com>
2023-11-24 20:20:59 +0100

    nvim/telescope: add shortcuts to search document and workspace symbols

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -6,4 +6,7 @@ vim.keymap.set('n', '<leader>fm', builtin.man_pages, {})
 vim.keymap.set('n', '<leader>fk', builtin.keymaps, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 
+vim.keymap.set('n', '<leader>fs', builtin.lsp_document_symbols, {})
+vim.keymap.set('n', '<leader>fS', builtin.lsp_workspace_symbols, {})
+
 vim.keymap.set('n', '<leader>fc', builtin.git_commits, {})
