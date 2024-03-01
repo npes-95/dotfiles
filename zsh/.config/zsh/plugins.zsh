056455c9a250a3db48f82b3dd32d81ad3b34d88b
npes-95 <npe.smith.95@gmail.com>
2024-03-01 23:21:42 +0100

    zsh: move plugins dir to .local/share

--- zsh/.config/zsh/plugins.zsh
+++ zsh/.config/zsh/plugins.zsh
@@ -1,17 +1,20 @@
-function zsh_add_file () {
-    [ -f "$zsh_config/$1" ] && source "$zsh_config/$1"
+zsh_plugin_dir="$HOME/.local/share/zsh/plugins"
+
+function try_source () {
+  [ -f "$1" ] && source "$1"
 }
 
 function zsh_add_plugin () {
-    plugin_name="$(echo $1 | cut -d "/" -f 2)"
-    alternative_name="$3"
-    if [ -d "$zsh_config/plugins/$plugin_name" ]; then
-        zsh_add_file "plugins/$plugin_name/$plugin_name.plugin.zsh" || \
-        zsh_add_file "plugins/$plugin_name/$plugin_name.zsh" || \
-        zsh_add_file "plugins/$plugin_name/$alternative_name.zsh"
-    else
-        git clone -b $2 "https://github.com/$1.git" "$zsh_config/plugins/$plugin_name"
-    fi
+  plugin_name="$(echo $1 | cut -d "/" -f 2)"
+  plugin_dir="$zsh_plugin_dir/$plugin_name"
+  alternative_name="$3"
+  if [ -d  "$plugin_dir" ]; then
+    try_source "$plugin_dir/$plugin_name.plugin.zsh" || \
+    try_source "$plugin_dir/$plugin_name.zsh" || \
+    try_source "$plugin_dir/$alternative_name.zsh"
+  else
+    git clone -b $2 "https://github.com/$1.git" "$plugin_dir"
+  fi
 }
 
 zsh_add_plugin zsh-users/zsh-syntax-highlighting 0.7.1
