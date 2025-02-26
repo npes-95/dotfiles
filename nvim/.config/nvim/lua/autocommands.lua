de2b4d029f417949e93f87444320ef7450f0e18b
npes-95 <npe.smith.95@gmail.com>
2025-02-26 17:34:09 +0100

    Revert "nvim: add autocmd to strip trailing whitespace in C/C++ files"
    This reverts commit f05dc63eb08d52ac422cad54ed14c8b5be9e7d94.

--- nvim/.config/nvim/lua/autocommands.lua
+++ nvim/.config/nvim/lua/autocommands.lua
@@ -23,13 +23,3 @@ autocmd("TermOpen", {
   group = personal,
   command = "setlocal listchars= nonumber norelativenumber"
 })
-
-autocmd("BufWritePre", {
-  pattern = { "*.h", "*.c", "*.cpp" },
-  group = personal,
-  callback = function(ev)
-    cursor_pos = vim.fn.getpos(".")
-    pcall(function() vim.cmd([[%s/\s\+$//e]]) end)
-    vim.fn.setpos(".", cursor_pos)
-  end,
-})
