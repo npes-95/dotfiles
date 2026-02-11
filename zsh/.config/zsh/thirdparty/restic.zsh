17bcc44ed45f83a3437307b2dc56254f08a453a1
npes-95 <npe.smith.95@gmail.com>
2026-02-11 12:12:02 +0100

    zsh: use hyphen for restic helper function names

--- zsh/.config/zsh/thirdparty/restic.zsh
+++ zsh/.config/zsh/thirdparty/restic.zsh
@@ -2,14 +2,14 @@ local repo_file="$HOME/.restic/repo"
 local exclude_file="$HOME/.restic/excludes"
 local include_file="$HOME/.restic/files"
 
-restic_backup () {
+restic-backup () {
   restic backup --repository-file="$repo_file" \
     --files-from="$include_file" \
     --exclude-file="$exclude_file" \
     --verbose 
 }
 
-restic_forget () {
+restic-forget () {
   restic forget --repository-file="$repo_file" \
     --host "$(hostname)" \
     --prune \
@@ -17,13 +17,13 @@ restic_forget () {
     --verbose
 }
 
-restic_du () {
+restic-du () {
   restic stats --repository-file="$repo_file" \
     --mode raw-data \
     --verbose
 }
 
-restic_ls () {
+restic-ls () {
   restic snapshots --repository-file="$repo_file" \
     --verbose
 }
