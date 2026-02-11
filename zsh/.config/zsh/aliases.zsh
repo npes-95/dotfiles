f65407529760c1d7d57c6552bdd9a078131957c4
npes-95 <npe.smith.95@gmail.com>
2026-02-11 12:14:02 +0100

    zsh: drop wtr alias

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -3,7 +3,6 @@ alias ls="ls --color"
 alias la="ls -la --color"
 alias du="du -ch"
 alias df="df -h"
-alias wtr="curl https://wttr.in/Berlin && printf '\n'"
 alias diff="diff --color=auto"
 
 has xdg-open && alias open="xdg-open"
